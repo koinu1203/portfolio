54770ab7a07cc2a0ac691ee0606c694148bb7e6c
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-10-13 22:44:36 -0500

    get local config

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { map, Subject, takeUntil } from 'rxjs';
+import { Languages } from './common/enums';
 import { DataService } from './common/services/data.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent implements OnInit {
-
-  DATA:any;
-  constructor(
-    private dataService:DataService
-  ) { }
+export class LandingPageComponent implements OnInit, OnDestroy {
+  DATA: any;
+  private unsubscribe$: Subject<void> = new Subject();
+  constructor(private dataService: DataService) {}
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 
   ngOnInit(): void {
-    this.getEnglishData();
+    this.loadConfig();
   }
-  getSpanishData(){
-    this.dataService.getSpanish()
-      .subscribe(val=>{
-        this.DATA=val;
+  loadConfig() {
+    this.dataService
+      .getConfig(Languages.ENGLISH)
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe((val) => {
+        this.DATA = val;
       });
-  }
-  getEnglishData(){
-    this.dataService.getEnglish()
-      .subscribe(val=>{
-        this.DATA=val;
-        console.log(this.DATA);
-      })
   }
 }
