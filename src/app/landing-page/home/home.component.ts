a89cb997b87d9c62a5eb32e87dbbb2afe76cc260
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-29 17:14:56 -0500

    title and subtitle added

--- src/app/landing-page/home/home.component.ts
+++ src/app/landing-page/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  title:string ='<!-- Daniel Ortiz -->';
+  subtitle:string ='<Front End Developer • Web Developer • Translator/>';
   constructor() { }
 
   ngOnInit(): void {
